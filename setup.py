e463c428f247716cb91260eee5993d978751f253
Md Nazrul Islam <connect2nazrul@gmail.com>
2025-06-23 11:17:45 +0200

    Preparing release 1.0.2

--- setup.py
+++ setup.py
@@ -101,7 +101,7 @@ setup(
         "dev": (test_requirements + development_requirements),
     },
     url="https://github.com/nazrulworld/fhir-core",
-    version="1.0.2.dev0",
+    version="1.0.2",
     zip_safe=False,
     python_requires=">=3.8",
     project_urls={
