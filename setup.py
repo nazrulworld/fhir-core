31805886f201fc1d90187f1b13516e903c3f7d4f
Md Nazrul Islam <connect2nazrul@gmail.com>
2025-07-11 20:11:05 +0200

    Back to development: 1.1.5

--- setup.py
+++ setup.py
@@ -101,7 +101,7 @@ setup(
         "dev": (test_requirements + development_requirements),
     },
     url="https://github.com/nazrulworld/fhir-core",
-    version="1.1.4",
+    version="1.1.5.dev0",
     zip_safe=False,
     python_requires=">=3.8",
     project_urls={
